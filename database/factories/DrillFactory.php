99a82e86ccb234a5f9fadf47d1d2be35ca65cf32
Joshua Gretton <jb.gretton@googlemail.com>
2024-08-26 14:06:52 +0100

    Migrations and Factories
    Drill and Category migrations and factories

---
+++ database/factories/DrillFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Drill>
+ */
+class DrillFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'=> User::factory(),
+            'title' => fake()->sentence(),
+            'small_description' => fake()->sentence(),
+            'description'=> fake()->text(),
+            'equipment'=> [
+                'key_1' => fake()->word(),
+                'key_2' => fake()->word(),
+                'key_3' => fake()->word(),
+            ],
+            'objectives'=> [
+                'key_1' => fake()->word(),
+                'key_2' => fake()->word(),
+                'key_3' => fake()->word(),
+            ],
+            'difficulty' => 'Advanced',
+            'duration' => fake()->numberBetween(0, 30),
+            'category_id'=>Category::factory(),
+            
+        ];
+    }
+}
---
+++ database/migrations/2024_08_26_110522_create_drills_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drills', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('small_description');
+            $table->text('description');
+            $table->json('equipment');
+            $table->json('objectives');
+            $table->string('difficulty');
+            $table->integer('duration');
+            $table->foreignIdFor(User::class);
+            $table->unsignedBigInteger('category_id')->nullable()->after('description'); // Adjust position as needed
+
+            // Set up foreign key constraint
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drills');
+        
+    }
+};

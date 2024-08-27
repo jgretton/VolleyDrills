6fb2eca0196dab5857b741f2addba380febee7d9
Joshua Gretton <jb.gretton@googlemail.com>
2024-08-27 21:36:10 +0100

    Create new category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -31,9 +32,18 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+
+        $validated = $request->validate([
+             'name'=> 'required|string|unique:categories',
+        ], [
+            'name.unique' => 'This category already exists'
+        ]);
+
+        Category::create($validated);
+        return redirect(route('dashboard.categories'));
+
     }
 
     /**
--- resources/js/Pages/Dashboard/Categories/Index.jsx
+++ resources/js/Pages/Dashboard/Categories/Index.jsx
@@ -1,9 +1,29 @@
+import Dropdown from "@/Components/Dropdown";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import SecondaryButton from "@/Components/SecondaryButton";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
-import React from "react";
+import { Head, useForm } from "@inertiajs/react";
+import React, { useState } from "react";
 
 export default function Index({ auth, categories }) {
-    console.log(categories);
+    const [creating, setCreating] = useState(false);
+
+    const { data, setData, processing, post, reset, errors, clearErrors } =
+        useForm({
+            name: "",
+        });
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(data);
+        post(route("categories.store"), {
+            onSuccess: () => {
+                reset();
+                setCreating(false);
+            },
+        });
+    };
     return (
         <Authenticated
             user={auth.user}
@@ -16,11 +36,55 @@ export default function Index({ auth, categories }) {
             <Head title="Categories" />
 
             <div className="max-w-7xl mx-auto py-12 px-6">
-                <div className="px-6 py-10 bg-white rounded-md">
-                    <ul>
+                <button onClick={() => setCreating((prevState) => !prevState)}>
+                    + Add new category
+                </button>
+                <div className="px-6 mt-10 py-10 bg-white rounded-md">
+                    <ul className="flex flex-col gap-4">
                         {categories.map((category, index) => (
-                            <li key={index}>{category.name}</li>
+                            <li
+                                key={index}
+                                className="flex flex-row max-w-sm items-center "
+                            >
+                                <p className="pl-4">{category.name}</p>
+                            </li>
                         ))}
+                        {creating ? (
+                            <form onSubmit={submit} className="">
+                                <label>New Category</label>
+                                <input
+                                    type="text"
+                                    value={data.name}
+                                    onChange={(e) =>
+                                        setData("name", e.target.value)
+                                    }
+                                    className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                />
+                                <InputError
+                                    message={errors.name}
+                                    className="mt-2"
+                                />
+                                <div className="space-x-2 mt-4">
+                                    <PrimaryButton disabled={processing}>
+                                        Save
+                                    </PrimaryButton>
+                                    <SecondaryButton
+                                        disabled={processing}
+                                        onClick={() => {
+                                            setCreating(
+                                                (prevState) => !prevState
+                                            );
+                                            reset();
+                                            clearErrors();
+                                        }}
+                                    >
+                                        Cancel
+                                    </SecondaryButton>
+                                </div>
+                            </form>
+                        ) : (
+                            ""
+                        )}
                     </ul>
                 </div>
             </div>
